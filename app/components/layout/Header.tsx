e78427128d60e0914a8b2985594934e2f43ab1e1
dawidlehai <haidawidle@gmail.com>
2023-01-30 14:25:58 +0100

    feat: add logo to the header and link it to the entry route

--- app/components/layout/Header.tsx
+++ app/components/layout/Header.tsx
@@ -1,8 +1,22 @@
 import type { LinksFunction } from "@remix-run/node";
+import { Link } from "@remix-run/react";
+import Logo from "../svg/Logo";
 import styles from "./Header.css";
 
 export default function Header() {
-  return <header>Header</header>;
+  return (
+    <header>
+      <div className="header__container">
+        <Link
+          to="/"
+          className="header__logo-link"
+          title="Powrót do strony głównej">
+          <Logo />
+        </Link>
+        Header
+      </div>
+    </header>
+  );
 }
 
 export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
