ec262af864aebff1a3ebec0ff286b6168cc81d5b
dawidlehai <haidawidle@gmail.com>
2023-01-30 11:18:52 +0100

    chore: export favicon links to other file

---
+++ app/faviconLinks.ts
@@ -0,0 +1,27 @@
+const faviconsPath = "img/favicons";
+
+const generateFavicons = (sizes: number[]) =>
+  sizes.map((size) => ({
+    rel: "icon",
+    type: "image/png",
+    href: `${faviconsPath}/favicon-${size}.png`,
+    sizes: `${size}`,
+  }));
+
+export default [
+  ...generateFavicons([16, 32, 96, 128, 196, 228]),
+  {
+    rel: "icon",
+    type: "image/svg+xml",
+    href: `${faviconsPath}/favicon.svg`,
+  },
+  {
+    rel: "apple-touch-icon",
+    href: `${faviconsPath}/apple-touch-icon.png`,
+    sizes: "180",
+  },
+  {
+    rel: "manifest",
+    href: "manifest.webmanifest",
+  },
+];
